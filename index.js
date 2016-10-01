e475e65b0e6084c19df94405858097c689ead9cb
Patatruc <xodrow@yahoo.com>
2016-10-01 22:07:09 +0200

    code splitted into parser.js (base class) and index.js (subclasses for each language)

--- index.js
+++ index.js
@@ -1,222 +1,109 @@
 
-var https = require("https");
-
-var titlesURL = ".wiktionary.org/w/api.php?action=query&list=search&format=json&utf8&srprop=&srsearch=";
-var pagesURL = ".wiktionary.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=";
+var parser = require("./parser.js");
 
 function getDef(word, lng, options, callback) {
 
 	if(typeof callback != "function") throw "word-definition error: no callback specified (getDef function).";
 	if(typeof options != "object") throw "word-definition error: options should be an object or null (getDef function).";
 
-	if(!word || /[\W\d_]/.test(stripAccents(word))) callback({ word: word, err: "invalid characters" });
-	else {
-		if(parseArticle[lng] == null) callback({ word: word, err: "unsupported language" });
-		else {
-			var obj = { word: word, lng: lng, options: options || {}, srwhat: "nearmatch", callback: callback };
-			getTitles(obj);
-		}
-	}
+	if(!word || /[\W\d_]/.test(parser.stripAccents(word))) callback({ word: word, err: "invalid characters" });
+	else if(!parsers[lng]) callback({ word: word, err: "unsupported language" });
+	else new parsers[lng](word, options, callback).getTitles();
 
 }
 
-function getTitles(obj) {
+var languages = [
 
-	var req = https.get("https://" + obj.lng + titlesURL + obj.word + "&srwhat=" + obj.srwhat, function(result) {
+	{
 
-		var cont = "";
+		lng: "en",
 
-		result.on("data", function(chunk) { cont += chunk; })
-		.on("end", function() { 
-			var articles = JSON.parse(cont).query.search;
-			if(articles.length) {
-				var exclude = obj.titles ? obj.titles[0] : "";
-				obj.titles = [];
-				var word2 = stripAccents(obj.word.toLowerCase());
-				articles.forEach(function(article) {
-					var title = article.title;
-					if (title != exclude && stripAccents(title.toLowerCase()) == word2) obj.titles.push(title);
-				});
-				if (obj.titles.length) getArticle(obj);
-				else obj.callback({ word: obj.word, err: "not found" });
-			}
-			else {
-				if(obj.srwhat == "nearmatch") {
-					obj.srwhat = "text";
-					getTitles(obj);
+		cats: "(Verb)|(Noun)|(Adjective)|(Adverb)|(Conjunction)|(Preposition)|" +
+			"(Determiner)|(Article)|(Pronoun)|(Interjection)",
+
+		variants: [
+			/^({{[^}]+}}\s*)*\[\[([^\]#|]+)[^\]]*\]\]\.*$/i,
+			/\s*{{[^|]+ of\|([^}|]+)/
+		],
+
+		searchDef: function(page) {
+
+			var def = "";
+
+			var match = new RegExp("===(" + (this.cat || this.cats) + ")===[^]+").exec(page);
+
+			if(match) {
+				var match2 = /\n{{((en-)|(head\|en)).*\n(\[\[[^\]]*\]\])*\n#(.+)(\n##(.+)){0,1}/.exec(match[0]);
+				if(match2) {
+					def = match2[5].trim();
+					this.cat = match[1];
+					if (match[6] && def.replace(/{{[^}]*}}/g, "").trim() == "") def = match2[6].trim();
 				}
-				else  obj.callback({ word: obj.word, err: "not found" });
 			}
-		});
-
-	});
-
-	req.on("error", function() { obj.callback({ word: obj.word, err: "a request has failed" }); });
-
-}
-
-function getArticle(obj) {
-
-	var req = https.get("https://" + obj.lng + pagesURL + encodeURIComponent(obj.titles[0]), function(result) {
-
-		var cont = "";
-		result.on("data", function(chunk) { cont += chunk;	})
-		.on("end", function() {
-			var pages = JSON.parse(cont).query.pages;
-			var page = pages[Object.keys(pages)[0]].revisions[0]["*"];
-			parseArticle[obj.lng](page, obj);
-		});
-	});
-	req.on("error", function() { obj.callback({ word: obj.word, err: "a request has failed" }); });
-
-}
-
-var parseArticle = {
-
-	en: function (page, obj) {
-
-		// ************ ENGLISH PARSER *********************
-		
-		var cats = "(Verb)|(Noun)|(Adjective)|(Adverb)|(Conjunction)|(Preposition)|(Determiner)|(Article)|(Pronoun)|(Interjection)";
-		var def = "";
-
-		var match = new RegExp("===(" + (obj.cat || cats) + ")===[^]+").exec(page);
-
-		if(match) {
-			var match2 = /\n{{((en-)|(head\|en)).*\n\n#(.+)(\n##(.+)){0,1}/.exec(match[0]);
-			if(match2) {
-				def = match2[4].trim();
-				if (match[6] && def.replace(/{{[^}]*}}/g, "").trim() == "") def = match2[6].trim();
-				if (def) {
-					obj.cat = match[1];
-					var redirect = /\s*{{[^|]+ of\|([^}|]+)/.exec(def);
-					if (redirect) {
-						obj.titles[0] = redirect[1];
-						getArticle(obj);
-						return;
-					}
-				}			
-			}
-		}
-
-		if (def) {
-			variante = /^({{[^}]+}}\s*)*\[\[([^\]#|]+)[^\]]*\]\]\.*$/i.exec(def);
-			if(variante) {
-				obj.titles[0] = variante[variante.length - 1];
-				getArticle(obj);
-				return;
-			}			
-			cleanup(obj, obj.titles[0].replace(/#.*/, ""), obj.cat.toLowerCase(), def);
-		}
-
-		else {
-			delete obj.cat;
-			if (obj.srwhat == "nearmatch") {
-				obj.srwhat = "text";
-				getTitles(obj);
-			}
-			else {
-				obj.titles.shift();
-				if(obj.titles.length) getArticle(obj);
-				else obj.callback({ word: obj.word, err: "not found" });
-			}
+			return def;
 		}
 	},
 
-	fr: function (page, obj) {
+	{
 
-		// ************ FRENCH PARSER *********************
-		
-		var def = "";
-		
-		var match = new RegExp("{{S\\|(" + (obj.cat || "[\\w\\s]+") +
-			")\\|fr(\\|flexion)*.+(\\n[^#].+)*\\n#(.+)(\\n##(.+)){0,1}").exec(page);
+		lng: "fr",
 
-		if(match) {
-			def = match[4].trim();
-			if (match[6] && def.replace(/{{[^}]*}}/g, "").trim() == "") def = match[6].trim();
-			if (def) {
-				obj.cat = match[1];
-				if (match[2]) {
-					var redirect = /[^\[]*\[\[([^\|\]]+)/.exec(def);
-					if (redirect) {
-						obj.titles[0] = redirect[1];
-						getArticle(obj);
-						return;
+		cats: "(nom)|(verbe)|(adjectif)|(adverbe)|(conjonction[^|]*)|" +
+			"(article[^|]*)|(pronom[^|]*)(interjection)|(préposition)|(onomatopée)",
+
+		variants: [
+			/{{variante [^|]*\|([^|}]+)/i,
+			/{{cf\|([^}]+)/i,
+			/((Variante)|(Autre)) [^\[]+\[\[([^\]#|]+)/i,
+			/^({{[^}]+}}\s*)*\[\[([^\]#|]+)[^\]]*\]\]\.*$/i
+		],
+		
+		searchDef: function(page) {
+
+			var def = "";
+			
+			var match = new RegExp("{{S\\|(" + (this.cat || this.cats) +
+				")\\|fr(\\|flexion)*.+(\\n[^#].+)*\\n#(.+)(\\n##(.+)){0,1}").exec(page);
+
+			if(match) {
+				var nMatches = match.length;
+				def = match[nMatches - 3].trim();
+				if (match[nMatches - 1] && def.replace(/{{[^}]*}}/g, "").trim() == "") def = match[nMatches - 1].trim();
+				if (def) {
+					this.cat = match[1];
+					if (match[nMatches - 5]) {
+						var redirect = /[^\[]*\[\[([^#\-|\]]+)/.exec(def);
+						if (redirect) {
+							this.titles[0] = redirect[1];
+							// console.log(this.word + " : inflection ==> " + redirect[1]);
+							this.word = redirect[1];
+							this.getPage();
+							def = true;
+						}
 					}
 				}
-				if(/^({{[^}]+}}\s*)*\.$/.test(def)) def = "";
 			}
-		}
-
-		if (def) {
-			var variante = /{{variante [^|]*\|([^|}]+)/i.exec(def);
-			if (!variante) variante = /{{cf\|([^}]+)/i.exec(def);
-			if (!variante) variante = /((Variante)|(Autre)) [^\[]+\[\[([^\]#|]+)/i.exec(def);
-			if (!variante) variante = /^({{[^}]+}}\s*)*\[\[([^\]#|]+)[^\]]*\]\]\.*$/i.exec(def);
-
-			if(variante) {
-				obj.titles[0] = variante[variante.length - 1];
-				getArticle(obj);
-				return;
-			}
-			def = def.replace(/{{lien\|([^\|]+)\|conv}}/g, "$1");
-			cleanup(obj, obj.titles[0].replace(/#.*/, ""), obj.cat, def );
-		}
-		else {
-			delete obj.cat;
-			if (obj.srwhat == "nearmatch") {
-				obj.srwhat = "text";
-				getTitles(obj);
-			}
-			else {
-				obj.titles.shift();
-				if(obj.titles.length) getArticle(obj);
-				else obj.callback({ word: obj.word, err: "not found" });
-			}
+			return def;
 		}
 	}
+]
 
-}
+var parsers = {};
 
-function cleanup(obj, word, cat, def) {
+languages.forEach(function(props) {
 
-	def = def.replace(/{{[^}]*}}/g, "").trim();
-	def = def.replace(/'''([^']+)'''/g, obj.options.formatted ? "<span style='bold'>$1</span>" : "$1");
-	def = def.replace(/''([^']+)''/g, obj.options.formatted ? "<span style='italic'>$1</span>" : "$1");
-
-	switch(obj.options.hyperlinks) {
-		case "brackets":
-			break;
-		case "html":
-			var url = "https://" + obj.lng + ".wiktionary.org/wiki/";
-			def = def.replace(/\[\[([^\]|]+)(\|)([^\]]+)\]\]/g,	"<a href='" + url + "$1' target='_blank'>$3</a>");
-			def = def.replace(/\[\[([^\]]+)\]\]/g,	"<a href='" + url + "$1' target='_blank'>$1</a>");
-			break;
-		case "none":
-		default:
-			def = def.replace(/\[\[([^\]|]+\|)*([^\]]+)\]\]/g, "$2");
+	var p = parsers[props.lng] = function(word, options, callback) {
+		this.base = parser.parser; 
+		this.base(word, props.lng, options, callback); 
 	}
 
-	obj.callback({ "word": word, "category": cat, "definition": def.trim() });
-}
+	p.prototype = new parser.parser;
 
-function stripAccents(text) {
+	var proto = p.prototype;
+	proto.cats = props.cats;
+	proto.variants = props.variants;
+	proto.searchDef = props.searchDef;
 
-	var replace = {
-		à: "a", á: "a", â: "a", ã: "a", ä: "a",
-		ç: "c",
-		è: "e", é: "e", ê: "e", ë: "e",
-		ì: "i", í: "i", î: "i", ï: "i",
-		ñ: "n",
-		ò: "o", ó: "o", ô: "o", õ: "o", ö: "o",
-		ù: "u", ú: "u", û: "u", ü: "u",
-		ý: "y", ÿ: "y"
-	}
-
-	for (var i = 0, len = text.length, res = ""; i < len; i++) res += replace[text[i]] || text[i];
-	return res;
-
-}
+});
 
 module.exports.getDef = getDef;
---
+++ parser.js
@@ -0,0 +1,157 @@
+
+var https = require("https");
+
+var titlesURL = ".wiktionary.org/w/api.php?action=query&list=search&format=json&utf8&srprop=&srsearch=";
+var pagesURL = ".wiktionary.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=";
+
+var parser = function(word, lng, options, callback) {
+
+	this.word = word;
+	this.lng = lng;
+	this.options = options || {};
+	this.callback = callback;
+	this.srwhat = "nearmatch";
+
+}
+
+var errors = {
+	notFound: "not found",
+	req: "a request has failed"
+}
+
+parser.prototype.sendErr = function(err, word) {
+
+	this.callback( { word: word || this.word, err: err });
+
+}
+
+parser.prototype.getTitles = function() {
+
+	var req = https.get("https://" + this.lng + titlesURL + this.word + "&srwhat=" + this.srwhat, function(result) {
+
+		var cont = "";
+
+		result.on("data", function(chunk) { cont += chunk; })
+		.on("end", function() { 
+			var articles = JSON.parse(cont).query.search;
+			if(articles.length) {
+				var exclude = this.titles ? this.titles[0] : "";
+				this.titles = [];
+				var word2 = stripAccents(this.word.toLowerCase());
+				articles.forEach(function(article) {
+					var title = article.title;
+					if (title != exclude && stripAccents(title.toLowerCase()) == word2) this.titles.push(title);
+				}, this);
+				if (this.titles.length) this.getPage();
+				else this.sendErr(errors.notFound);
+			}
+			else {
+				if(this.srwhat == "nearmatch") {
+					this.srwhat = "text";
+					this.getTitles();
+				}
+				else this.sendErr(errors.notFound);
+			}
+		}.bind(this));
+
+	}.bind(this));
+
+	req.on("error", function() { this.sendErr(errors.req) }.bind(this));
+
+}
+
+parser.prototype.getPage = function() {
+
+	var req = https.get("https://" + this.lng + pagesURL + encodeURIComponent(this.titles[0]), function(result) {
+		var cont = "";
+		result.on("data", function(chunk) { cont += chunk;	})
+		.on("end", function() {
+			var pages = JSON.parse(cont).query.pages;
+			var page = pages[Object.keys(pages)[0]].revisions[0]["*"];
+			this.parse(page);
+		}.bind(this));
+	}.bind(this));
+
+	req.on("error", function() { this.sendErr(errors.req) }.bind(this));
+
+}
+
+parser.prototype.parse = function(page) {
+
+	var def = this.searchDef(page);
+
+	if (def == true) return;
+
+	if (def) {
+		for(var i = 0, len = this.variants.length; i < len; i++) {
+			var found = this.variants[i].exec(def);
+			if(found) {
+				this.titles[0] = found[found.length - 1];
+				// console.log (this.word + " : variant #" + i + " ==> " + this.titles[0]);
+				this.getPage();
+				return;
+			}
+		}
+		this.cleanup(this.titles[0], this.cat.toLowerCase(), def);
+	}
+
+	else {
+		delete this.cat;
+		if (this.srwhat == "nearmatch") {
+			this.srwhat = "text";
+			this.getTitles();
+		}
+		else {
+			this.titles.shift();
+			if(this.titles.length) this.getPage();
+			else this.sendErr(errors.notFound); 
+		}
+	}
+
+}
+
+parser.prototype.cleanup = function(word, cat, def) {
+
+	def = def.replace(/{{[^}]*}}/g, "").trim();
+
+	if(/^\s*\.*$/.test(def)) return this.sendErr(errors.notFound, word);
+
+	def = def.replace(/'''([^']+)'''/g, this.options.formatted ? "<span style='bold'>$1</span>" : "$1");
+	def = def.replace(/''([^']+)''/g, this.options.formatted ? "<span style='italic'>$1</span>" : "$1");
+
+	switch(this.options.hyperlinks) {
+		case "brackets":
+			break;
+		case "html":
+			var url = "https://" + this.lng + ".wiktionary.org/wiki/";
+			def = def.replace(/\[\[([^\]|]+)(\|)([^\]]+)\]\]/g,	"<a href='" + url + "$1' target='_blank'>$3</a>");
+			def = def.replace(/\[\[([^\]]+)\]\]/g,	"<a href='" + url + "$1' target='_blank'>$1</a>");
+			break;
+		case "none":
+		default:
+			def = def.replace(/\[\[([^\]|]+\|)*([^\]]+)\]\]/g, "$2");
+	}
+
+	this.callback({ "word": word, "category": cat, "definition": def.trim() });
+}
+
+function stripAccents(text) {
+
+	var replace = {
+		à: "a", á: "a", â: "a", ã: "a", ä: "a",
+		ç: "c",
+		è: "e", é: "e", ê: "e", ë: "e",
+		ì: "i", í: "i", î: "i", ï: "i",
+		ñ: "n",
+		ò: "o", ó: "o", ô: "o", õ: "o", ö: "o",
+		ù: "u", ú: "u", û: "u", ü: "u",
+		ý: "y", ÿ: "y"
+	}
+
+	for (var i = 0, len = text.length, res = ""; i < len; i++) res += replace[text[i]] || text[i];
+	return res;
+
+}
+
+module.exports.parser = parser;
+module.exports.stripAccents = stripAccents;
